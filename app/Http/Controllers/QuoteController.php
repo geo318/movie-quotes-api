e2a1b7aa4b2d3be292554a06d99dc4182340749a
geo318 <geo.lomidze@gmail.com>
2023-01-03 14:44:18 +0400

    refactor: remove try catch

--- app/Http/Controllers/QuoteController.php
+++ app/Http/Controllers/QuoteController.php
@@ -3,33 +3,29 @@
 namespace App\Http\Controllers;
 
 use App\Models\Quote;
-use Throwable;
 
 class QuoteController extends Controller
 {
 	public function index()
 	{
-		try
+		if (count($quotes = Quote::paginate(3)->reverse()) === 0)
 		{
-			if (!count($quotes = Quote::paginate(3)->reverse()))
-			{
-				return response()->json([
-					'success' => 204,
-					'message' => 'no movies found',
-				]);
-			}
+			return response()->json([
+				'success' => 204,
+				'message' => 'no movies found',
+			]);
+		}
+		if (count($quotes) > 0)
+		{
 			return response()->json([
 				'success'    => 200,
 				'collection' => array_values($quotes->toArray()),
 			]);
 		}
-		catch(Throwable $e)
-		{
-			report($e);
-			return response()->json([
-				'error'   => 400,
-				'message' => 'something went wrong',
-			]);
-		}
+
+		return response()->json([
+			'error'   => 400,
+			'message' => 'something went wrong',
+		]);
 	}
 }
