0da3f9bcee17afd5558f0ed27396b77ca5a926db
geo318 <geo.lomidze@gmail.com>
2023-03-07 23:40:58 +0400

    refactor: user oauth login

--- app/Http/Controllers/GoogleController.php
+++ app/Http/Controllers/GoogleController.php
@@ -23,15 +23,14 @@ class GoogleController extends Controller
 		{
 			return response()->json(['message' => __('main.already_registered')], 422);
 		}
-		$user = User::updateOrCreate([
-			'gmail' => $provider_user->email,
-		], [
-			'username'          => $provider_user->name,
-			'gmail'             => $provider_user->email,
-			'email'             => $provider_user->email,
-			'avatar'            => $provider_user->avatar,
-			'email_verified_at' => Carbon::now(),
-		]);
+		$user = User::where(['gmail' => $provider_user->email])->first() ??
+			User::create([
+				'username'          => $provider_user->name,
+				'gmail'             => $provider_user->email,
+				'email'             => $provider_user->email,
+				'avatar'            => $provider_user->avatar,
+				'email_verified_at' => Carbon::now(),
+			]);
 
 		if (!Auth::login($user))
 		{
